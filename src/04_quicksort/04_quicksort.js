7e5f3f9112859c9d14cb439a362cbaf912c76368
Tayanne <tayh.snovaes@gmail.com>
2022-09-17 18:54:58 -0300

    add quicksort

---
+++ src/04_quicksort/04_quicksort.js
@@ -0,0 +1,11 @@
+const quickSort = (array) => {
+  if (array.length < 2) return array
+
+  const pivot = array[0]
+  const lowerThanPivot = array.slice(1).filter((num) => num <= pivot)
+  const biggerThanPivot = array.slice(1).filter((num) => num > pivot)
+
+  return [...quickSort(lowerThanPivot), pivot, ...quickSort(biggerThanPivot)]
+}
+
+console.log(quickSort([10, 5, 2, 3])) // [ 2, 3, 5, 10 ]
